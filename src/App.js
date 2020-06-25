39c6e4030ac41c367a80684dafafd9020b2faae3
Adeilton Sousa <adeiltonsousaxavier@gmail.com>
2020-06-25 15:34:45 -0300

    Adiçao de Estilos globaise das Libs (Axios, AntDesign, Styled-Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import GlobalStyle from "./styles/global.js";
+import "./styles/App.css";
 
 function App() {
   return (
-    <div>
-      <p>Mettzer</p>
-    </div>
+    <>
+      <GlobalStyle />
+    </>
   );
 }
 
--- src/styles/global.js
+++ src/styles/global.js
@@ -1,1 +1,21 @@
+import { createGlobalStyle } from "styled-components";
 
+const GlobalStyle = createGlobalStyle`
+  * {
+	  margin: 0;
+	  padding: 0;
+	  outline: 0;
+	  box-sizing: border-box;
+  }
+
+  body {
+	-webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font-family: 'Roboto', sans-serif;
+    -webkit-font-smoothing: antialiased;
+  }
+`;
+
+export default GlobalStyle;
