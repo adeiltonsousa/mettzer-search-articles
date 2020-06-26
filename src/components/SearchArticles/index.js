dbb55c23b10aa6e810e334307ecd6e3bcc39e9ff
Adeilton Sousa <adeiltonsousaxavier@gmail.com>
2020-06-26 11:59:23 -0300

    inclusao do InputNumber para controle do numero de Artigos por pagina

--- src/components/SearchArticles/index.js
+++ src/components/SearchArticles/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Input } from "antd";
+import { Input, InputNumber } from "antd";
 import api from "../../services/api";
 import { Container } from "./styles";
 import { useQuerySearch } from "../../store/querySearch";
@@ -8,29 +8,40 @@ import { useResultSearch } from "../../store/resultSearch";
 function SearchArticles() {
   const { resultSearch, setResultSearch } = useResultSearch("");
   const { query, setQuery } = useQuerySearch("");
+  const [PageSize, setPageSize] = useState(10);
+  const [page, setPage] = useState(1);
 
   const { Search } = Input;
 
   async function loadArticles(termSearch) {
     setQuery(termSearch);
-    const querySearch = query;
-    const page = 1;
-    const pageSize = 10;
     const apiKey = "EvLx9o8M4pI32OtkVX0Yri6HNZbnCJTA";
 
     await api
       .get(
-        `${termSearch}?page=${page}&pageSize=${pageSize}&urls=true&apiKey=${apiKey}`
+        `${termSearch}?page=${page}&pageSize=${PageSize}&urls=true&apiKey=${apiKey}`
       )
       .then((response) => setResultSearch(response.data))
       .catch((e) => {
         console.log(e);
       });
   }
+
+  function onChangeSizePage(value) {
+    setPageSize(value);
+  }
+
   console.log(resultSearch);
+  console.log(query);
 
   return (
     <Container>
+      <InputNumber
+        min={10}
+        max={100}
+        defaultValue={10}
+        onChange={onChangeSizePage}
+      />
       <Search
         placeholder="input search text"
         defaultValue="Brasil colonia"
